5ff1ae4474b82889603b7461bedc2a62485845d8
Leonid Pilyugin <l.pilyugin04@gmail.com>
2026-03-29 13:34:35 +0300

    a

--- kawaii/packages/cuda.scm
+++ kawaii/packages/cuda.scm
@@ -49,8 +49,7 @@
                     ;; https://github.com/Pang-Yatian/Point-MAE/pull/64/files).
                     #:key (gcc gcc) (linux-headers #f))
   (let* ((cuda>=11? (version>=? cuda-version "11"))
-         (cuda>=12.6? (version>=? cuda-version "12.6"))
-         (cuda>=13.1? (version>=? cuda-version "13.1")))
+         (cuda>=12.6? (version>=? cuda-version "12.6")))
     (package
       (name "cuda-toolkit")
       (version cuda-version)
@@ -323,7 +322,7 @@ libraries for NVIDIA GPUs, all of which are proprietary.")
   (make-cuda
     "13.1.1"
     (cuda-source
-     ""https://developer.download.nvidia.com/compute/cuda/13.1.1/local_installers/cuda_13.1.1_590.48.01_linux.run""
+     "https://developer.download.nvidia.com/compute/cuda/13.1.1/local_installers/cuda_13.1.1_590.48.01_linux.run""
      "1r5qvyv0sb8rwb9rb040x46v8739iyj95cq4d11q29vj4cvk5zr4")
     #:gcc gcc-15))
 
