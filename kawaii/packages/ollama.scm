51ce34c571ab16f55a28678b01b0870b0fa57d6a
Leonid Pilyugin <l.pilyugin04@gmail.com>
2026-04-04 12:42:30 +0300

    a

--- kawaii/packages/ollama.scm
+++ kawaii/packages/ollama.scm
@@ -3,6 +3,7 @@
   #:use-module (guix packages)
   #:use-module (guix git-download)
   #:use-module (guix build-system cmake)
+  #:use-module (guix build-system go)
   #:use-module ((guix licenses)
                 #:prefix license:))
 
@@ -29,6 +30,7 @@
       #~(modify-phases %standard-phases
         (delete 'check)
         (delete 'validate-runpath)
+        (add-after 'build 'build-go go-build)
       )))
     (home-page "https://ollama.com")
     (synopsis "Get up and running with large language models")
