3f0ee8804d5899c004f2ce014d5fa22223a870a6
Leonid Pilyugin <l.pilyugin04@gmail.com>
2026-04-11 20:59:48 +0300

    a

--- kawaii/services/autossh.scm
+++ kawaii/services/autossh.scm
@@ -54,7 +54,7 @@
   (shepherd-service
    (documentation "Automatically set up ssh connections (and keep them alive).")
    (provision '(autossh))
-   (requirement '(networking NetworkManager))
+   (requirement '(networking sing-box))
    (start #~(make-forkexec-constructor
              (list #$(file-append autossh "/bin/autossh")
                    #$@(autossh-configuration-ssh-options config))
