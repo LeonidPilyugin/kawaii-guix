3a597cfa1a713030924f4cca3c3b701ebe238ca8
Leonid Pilyugin <l.pilyugin04@gmail.com>
2026-05-20 09:25:25 +0300

    a

--- kawaii/packages/lutris.scm
+++ kawaii/packages/lutris.scm
@@ -7,6 +7,7 @@
   #:use-module (guix build-system gnu)
   #:use-module (gnu packages python)
   #:use-module (gnu packages glib)
+  #:use-module (guix-science-nonfree packages cuda)
   #:use-module (gnu packages python-xyz)
   #:use-module (gnu packages base))
 
